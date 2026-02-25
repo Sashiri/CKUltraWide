c18b8d82f812eac35a99ec6e0d6fa821de373be0
Sashiri <ali.reanc@gmail.com>
2026-02-25 16:58:09 +0100

    CK 1.2.0.3 Update * Use new `BurstDisabler` api, instead of synchronized job mirror * Fixes job unbursting due to `GatherTileUpdates_Job.tileOverridesLocal` field removal.

--- main.ultrawide.mod.cs
+++ main.ultrawide.mod.cs
@@ -1,85 +1,47 @@
 using HarmonyLib;
 using Pug.RP;
 using PugMod;
-using System;
 using System.Collections.Generic;
-using System.Reflection.Emit;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
 using Unity.Mathematics;
 
 namespace Sashiri
 {
-    [HarmonyPatch]
-    public class SendClientSubMapToPugMapSystem_GatherTileUpdates_Patch
-    {
-        public struct GatherTileUpdates_Job : IJob
-        {
-            public void Execute()
-            {
-                OriginalLambdaBody(ref this);
-            }
-
-            public NativeList<SendClientSubMapToPugMapSystem.LayerData> layerDataListLocal;
-            public NativeList<SendClientSubMapToPugMapSystem.TileUpdate> tileUpdatesLocal;
-            public NativeList<SendClientSubMapToPugMapSystem.TileOverride> tileOverridesLocal;
-            public float2 cameraPos;
-            public int2 subMapSize;
-        }
-
-        [HarmonyPatch("SendClientSubMapToPugMapSystem+GatherTileUpdates_Job", "OriginalLambdaBody")]
-        [HarmonyReversePatch]
-        static void OriginalLambdaBody(ref GatherTileUpdates_Job instance) => throw new NotImplementedException();
-    }
 
     [HarmonyPatch(typeof(SendClientSubMapToPugMapSystem))]
     static class SendClientSubMapToPugMapSystem_Patch
     {
-        [HarmonyPatch("GatherTileUpdates_Execute")]
-        [HarmonyPrefix]
-        static bool GatherTileUpdates_Execute(
-            NativeList<SendClientSubMapToPugMapSystem.LayerData> layerDataListLocal,
-            NativeList<SendClientSubMapToPugMapSystem.TileUpdate> tileUpdatesLocal,
-            NativeList<SendClientSubMapToPugMapSystem.TileOverride> tileOverridesLocal,
-            float2 cameraPos,
-            int2 subMapSize,
-            object __instance
-        )
-        {
-            var gatherTileUpdates_Job = new SendClientSubMapToPugMapSystem_GatherTileUpdates_Patch.GatherTileUpdates_Job
-            {
-                layerDataListLocal = layerDataListLocal,
-                tileUpdatesLocal = tileUpdatesLocal,
-                tileOverridesLocal = tileOverridesLocal,
-                cameraPos = cameraPos,
-                subMapSize = subMapSize,
-            };
+        static readonly System.Reflection.ConstructorInfo Int2Ctor =
+             typeof(int2).GetConstructor(new[] { typeof(int), typeof(int) });
 
-            var __base = (SystemBase)__instance;
-            __base.CheckedStateRef.Dependency = gatherTileUpdates_Job.Schedule(__base.CheckedStateRef.Dependency);
-            return false;
-        }
+        static readonly System.Reflection.MethodInfo HorizontalScaleMethod =
+            typeof(SendClientSubMapToPugMapSystem_Patch)
+                .GetMethod(nameof(ScaleHorizontalVisibilityView),
+                    System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
 
         [HarmonyPatch("ComputeShouldBeViewedMemo")]
         [HarmonyTranspiler]
 
         static IEnumerable<CodeInstruction> ComputeShouldBeViewedMemo(IEnumerable<CodeInstruction> instructions)
         {
+            // Find instructions resembling `int2 @int = new int2(20, 16);`
             var codeMatcher = new CodeMatcher(instructions);
             codeMatcher.MatchStartForward(
-                    new CodeMatch(OpCodes.Ldc_I4_S, (sbyte)20),
-                    new CodeMatch(OpCodes.Ldc_I4_S, (sbyte)16),
-                    new CodeMatch(OpCodes.Call, AccessTools.Constructor(typeof(int2), new[] { typeof(int), typeof(int) }))
-                );
-            if (codeMatcher.IsValid)
+                new CodeMatch(System.Reflection.Emit.OpCodes.Ldc_I4_S, (sbyte)20),
+                new CodeMatch(System.Reflection.Emit.OpCodes.Ldc_I4_S, (sbyte)16),
+                new CodeMatch(System.Reflection.Emit.OpCodes.Call, Int2Ctor)
+            );
+
+            if (!codeMatcher.IsValid)
             {
-                codeMatcher.Advance(1);
-                codeMatcher.InsertAndAdvance(new CodeInstruction(
-                    OpCodes.Call,
-                    AccessTools.Method(typeof(SendClientSubMapToPugMapSystem_Patch), nameof(ScaleHorizontalVisibilityView))
-                ));
+                throw new System.Exception("Sashiri.Ultrawide: Could not patch ComputeShouldBeViewedMemo, missing IL reference for view frustrum");
             }
+
+            // Skip instruction to pass default horizontal size to`HorizontalScaleMethod`
+            // return new value and continue the default flow 
+            codeMatcher.Advance(1);
+            codeMatcher.InsertAndAdvance(
+                new CodeInstruction(System.Reflection.Emit.OpCodes.Call, HorizontalScaleMethod)
+            );
 
             return codeMatcher.Instructions();
         }
@@ -104,6 +66,7 @@ namespace Sashiri
 
         public void Init()
         {
+            BurstDisabler.DisableBurstForSystemAndJobs<SendClientSubMapToPugMapSystem>();
             SetupCameraPreferences(API.Rendering.GameCamera);
             SetupCameraPreferences(API.Rendering.UICamera);
             DisableMenuBorders();
